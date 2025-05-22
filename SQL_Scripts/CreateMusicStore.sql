7b05d009f0cabe65bff9600817b794460c1d8e00
Adam W <dameart16@gmail.com>
2025-05-22 11:15:43 +0200

    Schemas added

--- SQL_Scripts/CreateMusicStore.sql
+++ SQL_Scripts/CreateMusicStore.sql
@@ -4,7 +4,14 @@ GO
 USE MusicStore; -- We use MusicStore and literally thats it
 GO
 
-CREATE TABLE dbo.Product (
+CREATE SCHEMA SalesManagement;
+GO 
+
+CREATE SCHEMA People;
+GO
+
+
+CREATE TABLE SalesManagement.Product (
     Id INT PRIMARY KEY IDENTITY(1,1), -- PK and Incrementation from1 every time by 1
     Name VARCHAR(60) NOT NULL,
     Category VARCHAR(20) NOT NULL,
@@ -13,7 +20,7 @@ CREATE TABLE dbo.Product (
 );
 GO
 
-CREATE TABLE dbo.[Order] (
+CREATE TABLE SalesManagement.[Order] (
     Id INT PRIMARY KEY IDENTITY(1,1),
     ProductId INT NOT NULL,
     CustomerId INT NOT NULL,
@@ -23,14 +30,14 @@ CREATE TABLE dbo.[Order] (
 );
 GO
 
-CREATE TABLE dbo.Customer (
+CREATE TABLE People.Customer (
     Id INT PRIMARY KEY IDENTITY(1,1),
     Name VARCHAR(20) NOT NULL,
     BirthDate DATETIME NOT NULL
 );
 GO
 
-CREATE TABLE dbo.Employee (
+CREATE TABLE People.Employee (
     Id INT PRIMARY KEY IDENTITY(1,1),
     Name VARCHAR(20) NOT NULL,
     BirthDate DATETIME NOT NULL,
@@ -38,11 +45,11 @@ CREATE TABLE dbo.Employee (
 );
 GO
 
-ALTER TABLE dbo.[Order] ADD CONSTRAINT FK_Order_Product FOREIGN KEY(ProductId) REFERENCES dbo.Product(Id);
-ALTER TABLE dbo.[Order] ADD CONSTRAINT FK_Order_Employee FOREIGN KEY(EmployeeId) REFERENCES dbo.Employee(Id);
-ALTER TABLE dbo.[Order] ADD CONSTRAINT FK_Order_Customer FOREIGN KEY(CustomerId) REFERENCES dbo.Customer(Id);
+ALTER TABLE SalesManagement.[Order] ADD CONSTRAINT FK_Order_Product FOREIGN KEY(ProductId) REFERENCES SalesManagement.Product(Id);
+ALTER TABLE SalesManagement.[Order] ADD CONSTRAINT FK_Order_Employee FOREIGN KEY(EmployeeId) REFERENCES People.Employee(Id);
+ALTER TABLE SalesManagement.[Order] ADD CONSTRAINT FK_Order_Customer FOREIGN KEY(CustomerId) REFERENCES People.Customer(Id);
 
-CREATE INDEX IX_Order_ProductId ON dbo.[Order](ProductId);
-CREATE INDEX IX_Order_Employee ON dbo.[Order](EmployeeId);
-CREATE INDEX IX_Order_Customer ON dbo.[Order](CustomerId);
-CREATE INDEX IX_Product_Category ON dbo.Product(Category); -- Probably frequently filtered column
+CREATE INDEX IX_Order_ProductId ON SalesManagement.[Order](ProductId); -- Faster working on FK (especially JOIN...) 
+CREATE INDEX IX_Order_Employee ON SalesManagement.[Order](EmployeeId);
+CREATE INDEX IX_Order_Customer ON SalesManagement.[Order](CustomerId);
+CREATE INDEX IX_Product_Category ON SalesManagement.Product(Category); -- Probably frequently filtered column
